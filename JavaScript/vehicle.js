f770611f13fc8c2292b41f1c478cfc8e4be3c728
sheterasika <sheterasika07@gmail.com>
2024-12-01 03:34:00 -1200

    fixed broken paths

--- JavaScript/vehicle.js
+++ JavaScript/vehicle.js
@@ -1,5 +1,5 @@
 let allimg  = [
-    "../img/car.png",'../img/splenderimg-removebg-preview.png','../img/motorcycle.png','../img/black-car.png','../img/black-motorcycle.png',
+    "../../img/car.png",'../../img/splenderimg-removebg-preview.png','../../img/motorcycle.png','../../img/black-car.png','../../img/black-motorcycle.png',
 ];
 let index = 0;
 let images = document.getElementById('new');
